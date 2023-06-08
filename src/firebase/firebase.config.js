387a832cc92f3786c6567d24f2c71ae64a55e756
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-06-08 16:07:48 +0600

    update

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHhPrI40M1LYBDQZBrogJ21O7FhAeZ3ic",
-  authDomain: "bangla-network.firebaseapp.com",
-  projectId: "bangla-network",
-  storageBucket: "bangla-network.appspot.com",
-  messagingSenderId: "170547480716",
-  appId: "1:170547480716:web:fbe2639ef9b83519eb93a9"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
