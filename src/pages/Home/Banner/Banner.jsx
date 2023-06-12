baaf7bee9933ed87a6ae7199e2050fc57944bcf4
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-06-12 13:03:38 +0600

    updfate

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,8 +1,55 @@
-import React from 'react';
+// import React from 'react';
+
+// const Banner = () => {
+//   return (
+//     <div className="bg-cover bg-center py-16 md:py-32 mt-20" style={{ backgroundImage: 'url(https://i.ibb.co/V94xFRC/1140-wi-fi-scams-web.jpg)' }}>
+// <div className="container mx-auto px-4 p-8 text-center bg-black bg-opacity-50 shadow-2xl rounded-2xl">
+//         <h1 className="text-4xl md:text-6xl text-white font-bold mb-4">Welcome to <span className='text-orange-400'>Bangla Network</span></h1>
+//         <h3 className='flex flex-wrap justify-center'>
+//           <span className='text-gray-300 text-3xl font-semibold'>
+//             Take Your Business &nbsp;
+//           </span>
+//           <span className='text-red-800 text-4xl font-semibold'>
+//             To The Next Leve
+//           </span>
+//         </h3>
+//         <p className="text-lg md:text-xl text-white mt-5">To build, construct, maintain, enlarge, pull down, remove or replace, dispose off, improve or develop and work, manage, and control any buildings, offices, godowns, warehouses, shops, machinery and plant and telephone exchanges, telex exchanges, message relay systems, microwave stationes, repeater stations, maintenance and operation of telecommunications services and conveniences.</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Banner;
+
+
+
+import React, { useState, useEffect } from 'react';
 
 const Banner = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const images = [
+    'https://i.ibb.co/V94xFRC/1140-wi-fi-scams-web.jpg',
+    'https://i.ibb.co/mRmFXwD/managed-wi-fi-solution.webp',
+    'https://i.ibb.co/qYw2Jfx/tips-jaringan-wifi.jpg',
+    'https://i.ibb.co/9h5KWDS/WIFI.jpg',
+    'https://i.ibb.co/bdbrPnF/WIFI.png',
+    'https://i.ibb.co/0cshXBG/all-network.jpg',
+    'https://i.ibb.co/NsdbZ6m/c-band-auction-80-b-connected-real-estate.png'
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 1500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="bg-cover bg-center py-16 md:py-32 mt-20" style={{ backgroundImage: 'url(https://i.ibb.co/D7JLDqz/IMG-1629.jpg)' }}>
+    <div
+      className="bg-cover bg-center py-16 md:py-32 mt-20"
+      style={{ backgroundImage: `url(${images[currentImage]})` }}
+    >
       <div className="container mx-auto px-4 p-8 text-center bg-black bg-opacity-50 shadow-2xl rounded-2xl">
         <h1 className="text-4xl md:text-6xl text-white font-bold mb-4">Welcome to <span className='text-orange-400'>Bangla Network</span></h1>
         <h3 className='flex flex-wrap justify-center'>
--- src/pages/Home/OurCompany/OurCompany.jsx
+++ src/pages/Home/OurCompany/OurCompany.jsx
@@ -7,20 +7,20 @@ const OurCompany = () => {
             <h2 className="text-center text-4xl mb-7 mt-7 text-white">Why Choose Bangla Network</h2>
             <div>
                 <div className="relative">
-                    <Parallax bgImage="https://i.ibb.co/nrjNc9r/wisp.jpg" strength={500}>
+                    <Parallax bgImage="https://i.ibb.co/4Ym85zt/WIFIZ.jpg" strength={500}>
                         <div className="h-screen bg-black bg-opacity-60">
                             <div className="flex flex-col justify-center items-center h-screen pt-20 pl-2 pr-2">
                                 <div className="container mx-auto py-12 flex flex-col lg:flex-row">
                                     <div className="lg:w-1/2">
                                         <div className="grid grid-cols-2 sm:grid-cols-2 gap-4">
                                             <div className="flex justify-center">
-                                                <img className="h-36 w-36 md:h-60 md:w-60 drop-shadow-2xl rounded-full shadow-2xl" src="https://images.unsplash.com/photo-1548174753-897b449b097e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80" alt="" />
+                                                <img className="h-36 w-36 md:h-60 md:w-60 drop-shadow-2xl rounded-full shadow-2xl" src="https://i.ibb.co/pr4h7tQ/kv-top-5g-sp.jpg" alt="" />
                                             </div>
                                             <div>
-                                                <img className="h-36 w-36 md:h-60 md:w-60 drop-shadow-2xl rounded-full shadow-2xl" src="https://images.unsplash.com/photo-1564038057903-1d48b852b412?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="" />
+                                                <img className="h-36 w-36 md:h-60 md:w-60 drop-shadow-2xl rounded-full shadow-2xl" src="https://i.ibb.co/F8HJjJD/fhgf.jpg" alt="" />
                                             </div>
                                             <div className="flex justify-center">
-                                                <img className="h-36 w-36 md:h-60 md:w-60 drop-shadow-2xl rounded-full shadow-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNYW2VNCva-76r3x-xJKugBl0IwODi3wpWWT-0RMboMNB9JfijhGGxo8x_K3w-FLu0-eA&usqp=CAU" alt="" />
+                                                <img className="h-36 w-36 md:h-60 md:w-60 drop-shadow-2xl rounded-full shadow-2xl" src="https://i.ibb.co/tzVhYyr/hhhhh.jpg" alt="" />
                                             </div>
                                             <div>
                                                 <img className="h-36 w-36 md:h-60 md:w-60 drop-shadow-2xl rounded-full shadow-2xl" src="https://img.freepik.com/premium-photo/tech-devices-icons-connected-digital-planet-earth_117023-449.jpg" alt="" />
@@ -30,7 +30,7 @@ const OurCompany = () => {
                                     <div className="lg:w-1/2 mt-12 lg:mt-0 lg:pl-12 shadow-2xl shadow-gray-800">
                                         <h2 className="text-xl md:text-3xl font-bold mb-4 text-white">There are several reasons why you should choose our company.</h2>
                                         <p className=" text-sm md:text-base text-gray-100">
-                                        Our system offers a comprehensive range of communication and telecommunication services, including basic/fixed line, cellular/mobile, wireless, and satellite services. We provide broadband services, data transmission, and advanced technologies like computer networks and intelligent networks. Our expertise extends to IP phone, WiFi, and internet solutions, including wireless tower installation and importing related goods. We excel in developing integrated solutions, software applications, and cloud-hosted business platforms. With a focus on emerging technologies, we offer services in areas such as data analytics, system administration, and enterprise mobility. Additionally, we provide outsourcing services and supply equipment for various industries. By choosing our system, you gain access to a wide range of services, cutting-edge technology, and comprehensive solutions tailored to your needs.
+                                        Our system offers a comprehensive range of communication and telecommunication services, including basic/fixed line, cellular/mobile, wireless, and satellite services. We provide broadband services, data transmission, and advanced technologies like computer networks and intelligent networks. Our expertise extends to IP phone, WiFi, and internet solutions, including wireless tower installation and importing related goods. We excel in developing integrated solutions, software applications, and cloud-hosted business platforms. By choosing our system, you gain access to a wide range of services, cutting-edge technology, and comprehensive solutions tailored to your needs.
                                         </p>
                                     </div>
                                 </div>
